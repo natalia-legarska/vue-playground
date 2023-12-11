3daefafff9079643241816a0be208c56c0ba3192
Natalia Legarska <natalia.legarska@euvic.pl>
2023-12-11 20:00:43 +0100

    Prettify code

--- vite.config.ts
+++ vite.config.ts
@@ -6,13 +6,13 @@ import vuetify from 'vite-plugin-vuetify'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vuetify()
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+    plugins: [
+        vue(),
+        vuetify()
+    ],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
     }
-  }
 })
